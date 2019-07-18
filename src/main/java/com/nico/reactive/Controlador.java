09fe8189c38357f0649bc5306612ce86dd08ccf1
Nicolas Pastor <nicolas.pastor@globant.com>
2019-07-18 16:55:45 -0400

    Mono Reactive Prog

--- src/main/java/com/nico/reactive/Controlador.java
+++ src/main/java/com/nico/reactive/Controlador.java
@@ -1,26 +1,35 @@
 package com.nico.reactive;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/controlador")
 public class Controlador {
 
+    ObjetoDominio objetoDominio;
+
     @GetMapping("/obtenerMono")
     public Mono<ObjetoDominio> obtenerMono(){
-
-        ObjetoDominio objetoDominio = new ObjetoDominio();
+        objetoDominio = new ObjetoDominio();
         objetoDominio.setNombre("nombre de prueba");
         objetoDominio.setValor("valor de prueba");
 
         System.out.println("---------------------------------------");
         System.out.println("paso por aca (provider)");
         System.out.println("---------------------------------------");
-
         return Mono.just(objetoDominio);
     }
 
+    public void setearMono(){
+        objetoDominio = new ObjetoDominio();
+
+        objetoDominio.setNombre("nombreeeeee");
+        objetoDominio.setValor("valor");
+
+    }
 }
